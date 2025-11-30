8408e5f98cfbd153f73161c2417a4228e3ee7568
Sigang Market <dev@sigang.com>
2025-11-30 18:04:26 +0900

    Deploy updated Flutter web app to GitHub Pages

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "692559766"
+    serviceWorkerVersion: "3308008045"
   }
 });
